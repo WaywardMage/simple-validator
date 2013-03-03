bcd90d1446650c1de4cf980237cc0b8b38fed73f
Daniel Hammond <viztheman@gmail.com>
2013-03-02 23:40:53 -0500

    Shook out some bugs and created demo pages to show the code in action.

--- bootstrap-validator.js
+++ bootstrap-validator.js
@@ -1,10 +1,10 @@
 ﻿(function ($) {
-	var _defaultTemplate = '<span class="help-inline"><i class="icon-warning-sign"></i></span>';
+    var _defaultTemplate = '<span class="help-inline"><i class="icon-warning-sign"></i></span>';
 
-	var _template = null;
-	var _rgxEmail = /^[0-9a-z._%+-]+@[0-9a-z.-]+\.[a-z]{2,6}$/i;
-	var _rgxUrl = new RegExp('^https?://[^.]+?(\.[^.]+?)*$', 'i');
-	
+    var _template = null;
+    var _rgxEmail = /^[0-9a-z._%+-]+@[0-9a-z.-]+\.[a-z]{2,6}$/i;
+    var _rgxUrl = new RegExp('^https?://[^.]+?(\.[^.]+?)*$', 'i');
+    
     // Stock validators.
     //
     var _validators = {
@@ -15,10 +15,10 @@
             var $this = $(this);
             
             if (value) {
-                var min = parseInt($this.data('stringlength-min'));
+                var min = parseInt($this.data('stringlength-min'), 10);
                 if (isNaN(min)) min = null;
 
-                var max = parseInt($this.data('stringlength-max'));
+                var max = parseInt($this.data('stringlength-max'), 10);
                 if (isNaN(max)) max = null;
 
                 if (min > 0 && max > 0 && (value.length < min || value.length > max)) {
@@ -38,17 +38,17 @@
             var $this = $(this);
             if (!value) return null;
             
-            var number = parseInt(value);
+            var number = parseInt(value, 10);
             if (isNaN(number)) return msg || 'Not a number.';
 
-            var min = parseInt($this.data('number-min'));
+            var min = parseInt($this.data('number-min'), 10);
             if (isNaN(min)) min = null;
 
-            var max = parseInt($this.data('number-max'));
+            var max = parseInt($this.data('number-max'), 10);
             if (isNaN(max)) max = null;
 
             if (min > 0 && max > 0 && (number < min || number > max)) {
-                return msg || "Must be between " + min.ToString() + " and " + max.toString() + ".";
+                return msg || "Must be between " + min.toString() + " and " + max.toString() + ".";
             }
             else if (min > 0 && number < min) {
                 return msg || "Must be " + min.toString() + " or greater.";
@@ -61,32 +61,33 @@
         },
         'regex': function(value, msg) {
             var pattern = $(this).data('regex-pattern');
+            var caseSensitive = $(this).data('regex-case-sensitive');
 
             if (pattern && value) {
-                var rgx = new RegExp(pattern);
+                var rgx = new RegExp(pattern, !caseSensitive ? 'i' : '');
                 if (!rgx.test(value)) return msg || "Invalid value.";
             }
 
             return null;
         },
         'match': function(value, msg) {
-            var selector = $(this).data('match-selector');
+            var selector = $(this).data('match-target');
             var $target = !selector ? null : $(selector);
                     
-            if ($target.length > 0 && value!==$target.val()) {
+            if ($target && $target.length > 0 && value!==$target.val()) {
                 return msg || "Values do not match.";
             }
 
             return null;
         },
         'email': function(value, msg) {
-            return _rgxEmail.test(value) ? null : (msg || "Not a valid email address.");
+            return !value || _rgxEmail.test(value) ? null : (msg || "Not a valid email address.");
         },
-		'url': function(value, msg) {
-			return _rgxUrl.test(value) ? null : (msg || "Not a valid URL.");
-		}
+        'url': function(value, msg) {
+            return !value || _rgxUrl.test(value) ? null : (msg || "Not a valid URL.");
+        }
     };    
-	
+    
     function getValueFromElement() {
         var $this = $(this);
 
@@ -109,13 +110,14 @@
 
             var msg = null;
             var customMsg = $this.data(name.toLowerCase() + '-msg');
+            var value = $.proxy(getValueFromElement, this)();
 
             if (_validators.stockNames.indexOf(name) < 0) {
-				var result = $.proxy(_validators[name], this)(value);
-				msg = result ? null : (customMsg || "Invalid value.";
-			}
+                var result = $.proxy(_validators[name], this)(value);
+                msg = result ? null : (customMsg || "Invalid value.");
+            }
             else
-				msg = $.proxy(_validators[name], this)(value, customMsg);
+                msg = $.proxy(_validators[name], this)(value, customMsg);
 
             if (msg) return msg;
         }
@@ -146,37 +148,37 @@
         if (msg) {
             $this.closest('.control-group').addClass('error');
 
-			var template = _template || _defaultTemplate;
+            var template = _template || _defaultTemplate;
             $err = $(template);
-			$err.addClass('validation-error-msg');
+            $err.addClass('validation-error-msg');
             $context.after($err);
             $err.popover({ title: 'Error', placement: 'right', trigger: 'hover', content: msg });
 
-			$this.addClass('validation-error');
+            $this.addClass('validation-error');
         }
         else {
             $this.closest('.control-group').removeClass('error');
-			$this.removeClass('validation-error');
+            $this.removeClass('validation-error');
         }
     }
 
     function validateElement(force) {
         var $this = $(this);
-		
-		if (
-			!force
-			&& $this.is('input, textarea, select')
-			&& !$this.data('validate-activated')
-			&& !$.proxy(getValueFromElement, this)()
-		)
-		{
-			return;
-		}
-		
+        
+        if (
+            !force
+            && $this.is('input, textarea, select')
+            && !$this.data('validate-activated')
+            && !$.proxy(getValueFromElement, this)()
+        )
+        {
+            return;
+        }
+        
         $this.data('validate-activated', true);
 
-		var msg = $.proxy(getValidationmessage, this)();
-		$.proxy(processErrorMsg, this)(msg);
+        var msg = $.proxy(getValidationMessage, this)();
+        $.proxy(processErrorMsg, this)(msg);
 
         return typeof msg === 'undefined' || msg === null;
     }
@@ -185,9 +187,9 @@
         $(this)
             .find('[data-validators]')
             .each(function () {
-				$(this).removeData('validate-activated');
-				$.proxy(processErrorMsg, this)();
-			})
+                $(this).removeData('validate-activated');
+                $.proxy(processErrorMsg, this)();
+            })
         ;
     }
 
@@ -195,13 +197,13 @@
         var $this = $(this);
         var success = true;
 
-		var findStr = erroredOnly ? '.validation-error' : '[data-validators]';
-		var $elements = $this.find(findStr);
+        var findStr = erroredOnly ? '.validation-error' : '[data-validators]';
+        var $elements = $this.find(findStr);
 
-		$.proxy(resetContainer, this)();
+        $.proxy(resetContainer, this)();
 
-		$elements.each(function () {
-			success = $.proxy(validateElement, this)(true) && success;
+        $elements.each(function () {
+            success = $.proxy(validateElement, this)(true) && success;
         });
 
         return success;
@@ -217,8 +219,8 @@
         if ($container.length < 1) return;
         
         $container.hasClass('validate')
-			? $.proxy(validateContainer, $container[0])()
-			: $.proxy(validateElement, $container[0])(true);
+            ? $.proxy(validateContainer, $container[0])()
+            : $.proxy(validateElement, $container[0])(true);
         ;
     }
 
@@ -235,21 +237,21 @@
             if (!value && $this.data('validation-held')) return;
         }
 
-		$.proxy(validateElement, this)();
+        $.proxy(validateElement, this)();
     }
 
     function onSubmit(e) {
-		if (!$.proxy(validateContainer, this)()) { 
+        if (!$.proxy(validateContainer, this)()) { 
             e.preventDefault();
-			e.stopImmediatePropagation();
-			return false;
+            e.stopImmediatePropagation();
+            return false;
         }
         else {
             return true;
-		}
+        }
     }
 
-    function addCustomValidator(name, fn) {
+    function addCustomValidators(name, fn) {
         if (!name) return;
 
         if (typeof name === 'object') {
@@ -267,31 +269,36 @@
     }
 
     $(document).ready(function () {
-		var stockNames = [];
-		
-		for (var i in _validators) {
-			stockNames.push(i);
-		}
-		_validators.__defineGetter__('stockNames', function() { return stockNames; });
-		
-		$(document).on('submit.validate', 'form.validate', onSubmit);
+        var stockNames = [];
+        
+        for (var i in _validators) {
+            stockNames.push(i);
+        }
+        _validators.stockNames = stockNames;
+        
+        $(document).on('submit.validate', 'form.validate', onSubmit);
         $(document).on('blur.validate', '.validate input[type!="hidden"], .validate select, .validate textarea', onBlur);
 
         $.validator = {
-            addCustomValidator: function() {
+            addCustomValidators: function() {
                 var options = Array.prototype.slice.call(arguments, 0);
-                addCustomValidator.apply(this, options);
+                addCustomValidators.apply(this, options);
             },
-			submitHandler: function(selector, fn) {
-				$(document).on('submit', selector, fn);
-			},
-			setCustomTemplate: function(html) {
-				_template = !html ? _defaultTemplate : html;
-				
-				$('.validate').each(function() {
-					$.proxy(validateContainer, this)(true);
-				});
-			}
+            submitHandler: function(selector, fn) {
+                $(document).on('submit', selector, function(e) {
+                    // Returning false doesn't QUITE work like it should,
+                    // so we help it out.
+                    //
+                    if (!fn(e)) e.preventDefault();
+                });
+            },
+            setCustomTemplate: function(html) {
+                _template = !html ? _defaultTemplate : html;
+                
+                $('.validate').each(function() {
+                    $.proxy(validateContainer, this)(true);
+                });
+            }
         };
 
         $.fn.validator = function (method) {
@@ -301,17 +308,17 @@
             this.each(function () {
                 switch (method.toLowerCase()) {
                     case 'validate':
-						result = !isContainer
-							? $.proxy(validateElement, this)()
-							: $.proxy(validateContainer, this)()
-						&& result;
+                        result = !isContainer
+                            ? $.proxy(validateElement, this)()
+                            : $.proxy(validateContainer, this)()
+                        && result;
                         break;
 
                     case 'reset':
-						isContainer
-							? $.proxy(resetContainer, this)()
-							: $.proxy(processErrorMsg, this)(null)
-						;
+                        isContainer
+                            ? $.proxy(resetContainer, this)()
+                            : $.proxy(processErrorMsg, this)(null)
+                        ;
                         break;
                 }
             });
